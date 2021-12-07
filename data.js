b134c0fdf3037cb1456985a96c5035e2f3233b2c
Taisiia Pomazan <taisiia.pomazan@aimprosoft.com>
2021-12-07 16:18:59 +0200

    update 1 task

---
+++ data.js
@@ -0,0 +1,53 @@
+const departments = [
+    {
+        id: 1,
+        name: 'Department 1',
+        employee: [
+            {
+                id: 1,
+                name: 'employee 1',
+                salary: 1000
+            },
+            {
+                id: 2,
+                name: 'employee 2',
+                salary: 2000
+            },
+            {
+                id: 3,
+                name: 'employee 3',
+                salary: 3500
+            },
+        ] 
+    },
+    {
+        id: 2,
+        name: 'Department 2',
+        employee: [
+            {
+                id: 4,
+                name: 'employee 4',
+                salary: 3000
+            },
+            {
+                id: 5,
+                name: 'employee 5',
+                salary: 4500
+            },
+            {
+                id: 6,
+                name: 'employee 6',
+                salary: 9000
+            },
+        ] 
+    },
+    {
+        id: 3,
+        name: 'Department 3',
+        employee: [
+            
+        ] 
+    },
+]
+
+export default departments
