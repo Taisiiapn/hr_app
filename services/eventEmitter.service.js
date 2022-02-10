c1c421aa5d20390700abfe237bda785ebb43bb8c
taisiia.pomazan <taisiia.pomazan@aimprosoft.com>
2022-02-10 16:41:20 +0200

    Methods with raw sql scripts were added to eventEmitter.service file

--- services/eventEmitter.service.js
+++ services/eventEmitter.service.js
@@ -1,8 +1,9 @@
 const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
-
+const { sequelize } = require('../model/department.model')
 const { Client } = require('pg')
-const environment = require('../config/environment')
+const environment = require('../config/environment');
+const { Logs } = require('../model/logs.model');
 
 const { port, host, user, password, database } = environment.db
 
@@ -23,6 +24,7 @@ function departmentToSQL_DTO (values) {
 } 
 
 function emoployeeToSQL_DTO (values) {
+    
     values
 } 
 
@@ -34,19 +36,20 @@ const events = {
     EMPLOYEE_VALIDATION_FAIL: 'EMPLOYEE_VALIDATION_FAIL',
 }
 
-myEmitter.on(events.DEPARTMENT_VALIDATION_FAIL, (error) => {
-
-    client.query(
-        `INSERT INTO logs(level, message) VALUES ('info', '${error}');`
-    )
-
+myEmitter.on(events.DEPARTMENT_VALIDATION_FAIL, async (error) => {
+    
+    await sequelize.query(`INSERT INTO logs(level, message) VALUES ('info', '${error}');`,
+        {  model: Logs}
+    );
+    
 })
 
 myEmitter.on(events.EMPLOYEE_VALIDATION_FAIL, (error) => {
 
-    client.query(
-        `INSERT INTO logs(level, message) VALUES ('info', '${error}');`
-    )
+    await sequelize.query(
+        `INSERT INTO logs(level, message) VALUES ('info', '${error}');`,
+        {  model: Logs}
+    );
 
 })
 
